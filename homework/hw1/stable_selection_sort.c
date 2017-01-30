90fbb3be841a573c80799b9e742d96ed46c205e6
Will <wldarden@gmail.com>
2017-01-29 19:12:58 -0600

    Task 1 written, not analyzed yet

--- homework/hw1/stable_selection_sort.c
+++ homework/hw1/stable_selection_sort.c
@@ -27,13 +27,13 @@ void print_data(struct record* d, int n_records);
 void unstable_sort(struct record* d, int n_records);
 void swap_record(struct record* d, int lowest, int space);
 struct record* copy_records(struct record* src, struct record* dest, int n_records);
+void stable_sort(struct record* d, int n_records);
 
 int main(void)
 {
   int n_records = 0;
   struct record* data;
   struct record* original_data;
-  printf("Original values: D: %p, size: %lu; OD: %p, size: %lu\n",data, sizeof(data), original_data, sizeof(original_data));
   char buffer[BUF_SIZE];
 
   printf("Enter the number of records:\n");
@@ -42,30 +42,26 @@ int main(void)
 
   original_data = read_data(n_records);
   data = copy_records(original_data, data, n_records);
-  // memcpy(&data, &original_data, sizeof(original_data));
-  // printf("After Load: D: %p, size: %lu; OD: %p, size: %lu\n",data, sizeof(data), original_data, sizeof(original_data));
+
   printf("Loaded data:\n");
   print_data(original_data, n_records);
+
   printf("Data sorted with UNSTABLE selection sort:\n");
   unstable_sort(original_data, n_records);
   print_data(original_data, n_records);
+  
   printf("A copy of the original data, sorted with STABLE selection sort:\n");
+  stable_sort(data, n_records);
   print_data(data, n_records);
-
-
 }
 
 struct record* copy_records(struct record* src, struct record* dest,int n_records){
   int i;
   dest = malloc(n_records*sizeof(struct record));
   for(i = 0; i < n_records; i ++){
-    // printf("During copy: D: %p, size: %lu; OD: %p, size: %lu\n",dest, sizeof(dest), src, sizeof(src));
-    printf("BEFORE di.%i, ds.%s, dp.%p; odi.%i, ods.%s, odp.%p\n",dest[i].i, dest[i].s, &dest[i].s, src[i].i,src[i].s, &src[i].s );
     dest[i].i = src[i].i;
     strcpy(dest[i].s, src[i].s);
-    printf("AFTER di.%i, ds.%s, dp.%p; odi.%i, ods.%s, odp.%p\n",dest[i].i, dest[i].s, &dest[i].s, src[i].i,src[i].s, &src[i].s );
   }
-  print_data(dest, n_records);
   return dest;
 }
 
@@ -86,6 +82,29 @@ void unstable_sort(struct record* d, int n_records){
     }
     if(lowest != space){// if the lowest element was changed, and the current space isnt the lowest it could be, swap them.
       swap_record(d,lowest,space);
+    }
+  }
+}
+
+void stable_sort(struct record* d, int n_records){
+
+  int space, lowest, i, j; //indexes, left hand, right hand, current iterator, move_down iterator
+
+  //find the lowest element for each space in array
+    //for each element check if its the lowest element.
+      //if it is save it as lowest element.
+    //Shift elements from space to lowest down one, put lowest up top
+  for(space = 0; space < (n_records - 1); space++){
+    lowest = space;
+    for(i = space + 1; i < n_records ; i++ ){// loop finds position of lowest element in array.
+      if(d[i].i < d[lowest].i){
+        lowest = i;
+      }
+    }
+    if(lowest != space){// if the lowest element was changed, and the current space isnt the lowest it could be, swap them.
+      for(j = lowest; j > space; j--){
+        swap_record(d,j,j-1);
+      }
     }
   }
 }
